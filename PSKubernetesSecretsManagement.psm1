d0f7b243d4a6bdf8da611a17bc602a634f1b757b
Tony Guimelli <tony.guimelli@gmail.com>
2023-08-16 13:18:26 -0400

    Add files via upload

--- PSKubernetesSecretsManagement.psm1
+++ PSKubernetesSecretsManagement.psm1
@@ -4,15 +4,23 @@
 #>
 
 
-#region Load Private Functions
+
+# Prerequisites:
+
+try {
+    Get-Command -Name kubectl -ErrorAction Stop | Out-Null
+}
+catch {
+    $FileNotFoundException = [IO.FileNotFoundException]::new("Required binary kubectl not found in path. Module import failed.")
+    throw $FileNotFoundException
+}
+
+# Load Private Functions:
 
 Get-ChildItem -Path $PSScriptRoot\PrivateFunctions\*.ps1 | Foreach-Object { . $_.FullName }
 
-#endregion
 
 
-#region Load Public Functions
+# Load Public Functions:
 
 Get-ChildItem -Path $PSScriptRoot\Functions\*.ps1 | Foreach-Object { . $_.FullName }
-
-#endregion
